706285a3c796981ed95986bf817ab4fcab84a073
Trond Marius Øvstetun <trond@ovstetun.no>
2011-05-09 08:32:25 +0200

    newline

--- project/build/ScalaJava.scala
+++ project/build/ScalaJava.scala
@@ -1,6 +1,7 @@
 import sbt._
 
 class ScalaJava(info: ProjectInfo) extends ParentProject(info) {
+
 	lazy val persistens = project("persistens", "persistens", new DefaultProject(_) with Dependencies {
     val hibernate = "org.hibernate" % "hibernate-entitymanager" % "3.6.1.Final"
     val hsql = "org.hsqldb" % "hsqldb" % "2.0.0"
